64c29167d4b2c20221d11943ff20e5fa5ad9915f
aliiexe <alibusinessbourak@gmail.com>
2024-05-21 18:05:54 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Landing/Hero"
 import Feature from "../components/Landing/Feature"
 import Footer from "../components/Landing/Footer"
+import About from "../components/Landing/About"
 
 export default function Landing() {
   return (
     <div>
       <Hero />
       <Feature />
+      <About />
       <Footer />
     </div>
   )
