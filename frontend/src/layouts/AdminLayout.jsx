43e4a41cc8ff143196864a63ebc4fda4c44e02e8
aliiexe <alibusinessbourak@gmail.com>
2024-05-23 12:54:46 +0100

    aaaaa

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -36,7 +36,6 @@ export default function AdminLayout(){
         getItem(<Link to="/dashboard">Dashboard</Link>, '1', <PieChartOutlined style={{"paddingTop":"0px !important"}}/>),
         getItem(<Link to="/intervenant">Intervenant</Link>, '2', <TeamOutlined />),
         getItem(<Link to="/formation">Formations</Link>, '3', <ShopOutlined />),
-        getItem(<Link to="/region">Region</Link>, '4', <ShopOutlined />),
     ];
 
     
@@ -51,7 +50,6 @@ export default function AdminLayout(){
     >
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div className="demo-logo-vertical" style={{display:"flex",justifyContent:"center"}}><Link> <div style={{display:"flex",flexDirection:"column",padding:"5px",justifyContent:"center"}}><img src={"/images/ofppt-logo.png"} style={{height:"5%", marginTop:"20px"}}></img><span style={{color:"white",textAlign:"center",marginTop:"10px"}}> Espace Admin</span></div></Link></div>
-        <div className="demo-logo-vertical" style={{display:"flex",justifyContent:"center"}}><Link> <div style={{display:"flex",flexDirection:"column",padding:"5px",justifyContent:"center"}}><img src={"/images/ofppt-logo.png"} style={{height:"5%", marginTop:"20px"}}></img><span style={{color:"white",textAlign:"center",marginTop:"10px"}}> Espace Admin</span></div></Link></div>
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
       </Sider>
       <Layout>
@@ -60,11 +58,6 @@ export default function AdminLayout(){
             padding: 0,
             background: colorBgContainer,
           }}
-        >
-          <div style={{display:"flex"}}>
-            {/* <img src={"/images/ofppt-logo.png"} style={{"height":"10%"}}></img>Espace Admin */}
-            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,288L48,288C96,288,192,288,288,250.7C384,213,480,139,576,128C672,117,768,171,864,176C960,181,1056,139,1152,149.3C1248,160,1344,224,1392,256L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg> */}
-          </div></Header>
         >
           <div style={{display:"flex"}}>
             {/* <img src={"/images/ofppt-logo.png"} style={{"height":"10%"}}></img>Espace Admin */}
@@ -81,13 +74,9 @@ export default function AdminLayout(){
               // color:"white",
               fontSize:"16px",
               fontWeight:"bold"
-              // color:"white",
-              fontSize:"16px",
-              fontWeight:"bold"
             }}
           >
             <Breadcrumb.Item>Admin</Breadcrumb.Item>
-            <Breadcrumb.Item><span>{location.pathname.split('/')[1]}</span></Breadcrumb.Item>
             <Breadcrumb.Item><span>{location.pathname.split('/')[1]}</span></Breadcrumb.Item>
           </Breadcrumb>
           <div
@@ -102,13 +91,11 @@ export default function AdminLayout(){
           </div>
         </Content>
         {/* <Footer
-        {/* <Footer
           style={{
             textAlign: 'center',
           }}
         >
          YourLibrary
-        </Footer> */}
         </Footer> */}
       </Layout>
     </Layout>
@@ -116,5 +103,4 @@ export default function AdminLayout(){
         </>
     )
 }
-
 
